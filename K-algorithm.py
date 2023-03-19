4a9aace0d8c65cafc7660607f3105d9db1a56c47
tomek19991999 <tomek19991999@gmail.com>
2023-03-19 14:51:36 +0100

    new file:   .vscode/launch.json 	new file:   K-algorithm.py 	new file:   collections 	new file:   iris.csv 	new file:   iris.names

---
+++ K-algorithm.py
@@ -0,0 +1,229 @@
+from csv import reader
+from math import sqrt
+from collections import Counter
+
+#Loading from CSV file
+def loading_csv (file):
+    data=list()
+    with open(file, 'r') as file:
+        csvreader = reader(file)
+        for row in csvreader:
+            if not row:
+                continue
+            data.append(row)
+
+    #make flaot data (before, we had string data)
+    for row in data:
+        for i in range(len(data[0])-1):
+            row[i]=float(row[i])
+            #print(row[i])
+    return data
+    
+
+#Data visualization
+def data_visualisation():
+    plt.figure(figsize=(8,7))
+    color = ['red', 'green', 'blue']
+    labels= ['setosa', 'versicolor', 'virginica']
+    for row in range(len(data)): #put data to scattler plot
+        if row<50:
+            plt.scatter(data[row][2],data[row][3],c=color[0], label='setosa')
+        elif row<100:
+            plt.scatter(data[row][2],data[row][3],c=color[1], label='versicolor')
+        else:
+            plt.scatter(data[row][2],data[row][3],c=color[2], label='virginica')
+
+    plt.legend()
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.title('Scatter plot of IRIS flowers')
+    plt.show()
+
+#converting list to values from 1 to 0
+def convert_to_values_from_one_to_zero(data):
+    max_list=list()
+    min_list=list()
+    #taking maximum and minimum values
+    for i in range (len(data[0])-1):
+        a=max(l[i] for l in data) #take maxiumum from column "i"
+        max_list.append(a)
+        a=min(l[i] for l in data)
+        min_list.append(a)
+    #print("max:",max_list)
+    #print("min:",min_list)
+    
+    #rescale our values from data
+    for row in data:
+        for i in range(len(row)-1):
+            row[i] = (row[i]-min_list[i])/(max_list[i]-min_list[i])
+    #print (data[0])
+    return data
+
+#calculate Euclidean distance
+def euclidean_distance(row1,row2):
+    distance=0.0
+    for i in range (len(row1)-1):
+        distance += (row1[i] - row2[i])**2 #euclidean distance = sqrt[(a1-a2)^2 + (b1-b2)^2 + (c1-c2)^2 + (d1-d2)^2 + .....]
+    return sqrt(distance)
+
+#get list of neighbors
+def get_list_of_neighbors(data,test_sample,k):
+    #calculating distance from all neighbors and make list with it (150 rows, 1 column)
+    distance_list=list()
+    for row in range (len(data)):
+        distance_list.append(euclidean_distance(data[row],test_sample)) #make list with distances
+    #print (distance_list)
+    new_big_distance_list = [a + [b] for a, b in zip(data, distance_list)] #merge data and distance lists, format of big list: [a,b,c,d,group_name,distance] (with all data:150samples)
+    new_big_distance_list.sort(key=lambda my_list:my_list[5])
+    small_distance_list=new_big_distance_list[0:k] #take k elements to new, small list 
+    #print(small_distance_list)
+    return small_distance_list
+
+#choose group for our test sample
+def choose_final_group_for_test_sample(nearest_neighbors):
+    #making list with only names of neighbors
+    list_of_groups=list()
+    for row in nearest_neighbors:
+        list_of_groups.append(row[4])
+    #searching for most string with biggest frequency
+    word_count = Counter(list_of_groups) #make object with counted groups
+    most_common_word = word_count.most_common(1)[0][0] #take world with biggest frequency
+    #print (most_common_word)
+    return most_common_word
+
+#take from data: 60% training samples, 20% walidation samples, 20% test samples
+def take_training_data(data):
+    training_data=list()
+    #calculate how many samples take
+    how_many_training_data=0.6*len(data) #output 90
+    #print(how_many_samples_take)
+
+    #taking data
+    for i in range (int(how_many_training_data/3)):
+        training_data.append(data[i])    #0-49
+        training_data.append(data[i+50]) #50-99
+        training_data.append(data[i+100])#100-149
+    #print(len(training_data))
+    #print(training_data)
+    return training_data
+
+def take_validation_data(data):
+    validation_data=list()
+    #calculate how many samples take
+    how_many_validation_data=0.2*len(data) #output 30
+    #print(how_many_samples_take)
+
+    #taking data
+    for i in range (int(how_many_validation_data/3)):
+        validation_data.append(data[i+30])
+        validation_data.append(data[i+80])
+        validation_data.append(data[i+130])
+    return validation_data
+
+def take_test_data(data):
+    test_data=list()
+    #calculate how many samples take
+    how_many_data=0.2*len(data) #output 30
+    #print(how_many_samples_take)
+
+    #taking data
+    for i in range (int(how_many_data/3)):
+        test_data.append(data[i+40])
+        test_data.append(data[i+90])
+        test_data.append(data[i+140])
+
+    #delete iris names column
+    for j in test_data:
+        del j[4]
+    #print(test_data)
+    return test_data
+
+def validation_data_test (training_data,validation_data,k):
+    nearest_neighbors=list()
+    counter_correct_assign=0
+    for row in range (len(validation_data)):
+        nearest_neighbors=get_list_of_neighbors(training_data,validation_data[row],k)
+        final_group=choose_final_group_for_test_sample(nearest_neighbors)
+        if final_group==validation_data[row][4]:
+            counter_correct_assign += 1
+            #print("Poprawnie przydzielono probke nr:",row)
+    #print (counter_correct_assign)
+    return counter_correct_assign
+
+
+
+def k_nearest_neighbors(file_name,k,auto_find_k):
+
+
+    data=loading_csv(file_name)
+    data = convert_to_values_from_one_to_zero(data)
+
+    training_data=list()
+    validation_data=list()
+    test_data=list()
+
+    training_data=take_training_data(data)
+    validation_data=take_validation_data(data)
+    test_data=take_test_data(data)
+    #training_data.append(validation_data[0]) #test
+
+    # If K autofind is ON (1)
+    if auto_find_k==1:
+        max_result=0 #max output validation data test funcion (how many samples is assigned correctly)
+        result=0
+        k_result=0 #best k
+        
+        for i in range (1,len(training_data)):
+            result=validation_data_test(training_data,validation_data,i) #return number of correct validations
+            if max_result<result:
+                max_result=result
+                k_result=i
+        print ("max wynik:",max_result, "przy k=",k_result)
+
+    # If K autofind is OFF (0)
+    else:
+        result=(validation_data_test(training_data,validation_data,k))
+        print("numbers of correct validations=",result)
+
+    
+
+
+
+####initialising variables
+k=5
+file_name="iris.csv"
+##### fuseBit autofind best K
+auto_find_k=1 # 0-OFF, 1-ON
+
+k_nearest_neighbors(file_name,k,auto_find_k)
+
+
+"""
+data=loading_csv(file_name)
+data = convert_to_values_from_one_to_zero(data)
+print(data)
+
+test_sample=list()
+training_data=list()
+validation_data=list()
+test_data=list()
+
+training_data=take_training_data(data)
+validation_data=take_validation_data(data)
+test_data=take_test_data(data)
+
+nearest_neighbors=get_list_of_neighbors(data,validation_data,k)
+
+test_sample.append(data[1])#USUNAC Z TEJ TESTOWEJ PROBKI NAZWE GRUPY
+#test_sample=[5.0, 4.0, 2.0, 0.5, 'Iris-setosa']
+training_data=take_training_data(data)
+print("TEST SAMPLE: ",test_sample[0])
+data = convert_to_values_from_one_to_zero(data)
+test_sample=convert_to_values_from_one_to_zero(test_sample)
+print("TEST SAMPLE2: ",test_sample)
+#test_sample=[0.6, 0.2, 0.13, 0.72, 'Iris-setosa']
+nearest_neighbors=get_list_of_neighbors(data,test_sample,k)
+#print(nearest_neighbors)
+final_group=choose_final_group_for_test_sample(nearest_neighbors)
+print(final_group)
+"""
